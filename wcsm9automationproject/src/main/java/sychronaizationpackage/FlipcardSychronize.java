3e28601898e46e08b0a15d758018337f6c5999a4
Shubham Gaikwad <Shubham Gaikwad@DESKTOP-0G2S0KD>
2023-10-21 12:55:05 +0530

    first commit

---
+++ wcsm9automationproject/src/main/java/sychronaizationpackage/FlipcardSychronize.java
@@ -0,0 +1,27 @@
+package sychronaizationpackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipcardSychronize {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		//apply implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+		driver.get("https://www.flipkart.com/");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//span[text()='✕']")).click();
+		driver.findElement(By.xpath("")).click();
+		
+        
+
+      
+	}
+
+}
